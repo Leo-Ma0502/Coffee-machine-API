27c3582c3968d6769a8231036e255dfaaec267ef
yeheng.ma <Myh_00@outlook.com>
2024-05-11 15:23:17 +1200

    add parameter validation middleware

--- CoffeeMachineAPI/Program.cs
+++ CoffeeMachineAPI/Program.cs
@@ -2,6 +2,7 @@ using CoffeeMachineAPI.Middleware;
 using CoffeeMachineAPI.Model;
 using CoffeeMachineAPI.Service;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IDateTimeService, DateTimeService>();
@@ -11,16 +12,15 @@ builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
+app.UseParameterValidation();
 app.UseExceptionHandling();
 
-app.MapGet("/brew-coffee", async (HttpContext http, ICoffeeService coffeeService, [FromQuery] double? lat, [FromQuery] double? lon) =>
+app.MapGet("/brew-coffee", async (HttpContext http, ICoffeeService coffeeService, [FromQuery] string? lat, [FromQuery] string? lon) =>
 {
-    // the updated endpint records latitude and longitude in the params
-    var location = new Location
-    {
-        Lat = lat,
-        Lon = lon
-    };
+    // the updated endpint takes latitude and longitude in the params
+    // get location from the middleware
+    Location? location = http.Items.TryGetValue("Location", out var locationObj) ? locationObj as Location : null;
+
     // the updated BrewCoffee method takes the location as parameter, as required by the third-party API
     var response = await coffeeService.BrewCoffee(location);
     http.Response.StatusCode = response.StatusCode;
